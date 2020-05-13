b336bd2d28abfc2af8b80e8230d8e5c158ead72c
daovanduc <duc07092000@gmail.com>
2020-05-13 18:03:16 +0700

    login

--- controller/ControllerUsers.php
+++ controller/ControllerUsers.php
@@ -6,17 +6,30 @@ use Model\UsersDB;
 
 class ControllerUsers
 {
-    protected $users;
+    protected $usersDB;
 
     public function __construct()
     {
-
+        $this->usersDB = new UsersDB();
     }
 
     function login()
     {
         if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            // lay du lieu tu form login
+            $email = $_REQUEST['email'];
+            $password = $_REQUEST['password'];
+            // check voi tren csdl
+            $checkLogin = $this->usersDB->login($email, $password);
 
+            if ($checkLogin) {
+                $_SESSION['login'] = $checkLogin;
+                // cho phep vao home
+                header('location: index.php');
+            } else {
+                $_SESSION['errorLogin'] = 'email or password incorrect';
+                header('location: index.php');
+            }
         }
     }
 }
--- model/UsersDB.php
+++ model/UsersDB.php
@@ -4,7 +4,22 @@
 namespace Model;
 
 
-class UsersDB
+class UsersDB extends LibraryDB
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->nameTable = 'users';
+    }
 
+    public function login($email, $password)
+    {
+        $sql = "SELECT * FROM ? WHERE `email`= ? AND `password` = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(1, $this->nameTable);
+        $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $password);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 }
