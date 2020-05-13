7a16525ea9f24a63a4c37793faee568e7286a4aa
daovanduc <duc07092000@gmail.com>
2020-05-13 17:54:40 +0700

    update

--- controller/ControllerUsers.php
+++ controller/ControllerUsers.php
@@ -1,7 +1,22 @@
 <?php
 
+namespace Controller;
+
+use Model\UsersDB;
 
 class ControllerUsers
 {
+    protected $users;
 
+    public function __construct()
+    {
+
+    }
+
+    function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+        }
+    }
 }
