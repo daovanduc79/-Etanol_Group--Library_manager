ba0406cb3d9d5264b3048def4871ed27f5c6c857
daovanduc <duc07092000@gmail.com>
2020-05-13 16:06:45 +0700

    add class Users

--- class/Users.php
+++ class/Users.php
@@ -2,7 +2,23 @@
 
 namespace Library;
 
+use Model\LibraryDB;
+
 class Users
 {
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+    public $phone;
+
+    public function __construct($id, $name, $email, $password, $phone)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->phone = $phone;
+    }
 
 }
