816b048b8321b96daf8b4440f4726b62a29b9da2
daovanduc <duc07092000@gmail.com>
2020-05-13 15:27:16 +0700

    add ConnectionDB

---
+++ model/ConnectionDB.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Model;
+
+use PDO;
+use PDOException;
+
+
+class ConnectionDB
+{
+    protected $server;
+    protected $username;
+    protected $password;
+
+    protected function __construct()
+    {
+        $this->server = "mysql:host=localhost;dbname=case_study_md2";
+        $this->username = "root";
+        $this->password = "123456@Abc";
+    }
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO($this->server, $this->username, $this->password);
+        } catch (PDOException $e) {
+            $e->getMessage();
+            exit();
+        }
+    }
+}
